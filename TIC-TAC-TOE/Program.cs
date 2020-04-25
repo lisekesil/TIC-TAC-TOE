1d93a83777132d5b9104ba4cf0e1ee9b94bbd293
Wojtek Lisowicz <lisekmadridista@gmail.com>
2020-04-25 19:19:05 +0200

    added function for drawing the board

--- TIC-TAC-TOE/Program.cs
+++ TIC-TAC-TOE/Program.cs
@@ -17,11 +17,37 @@ namespace TIC_TAC_TOE
                 {'4','5', '6' },
                 {'7','8', '9' }
             };
+            char[,] boardCopy = board.Clone() as char[,];
 
             Console.Write("player A name: ");
             namePlayerA = Console.ReadLine();
             Console.Write("player B name: ");
             namePlayerB = Console.ReadLine();
+
+            bool endGame = false;
+            for(int round = 0; round < board.Length; ++round)
+            {
+                Console.Clear();
+                DrawBoard(board);
+
+                Console.ReadKey();
+                //...
+            }
+        }
+
+        static void DrawBoard(char[,] board)
+        {
+            int height = board.GetLength(0);
+            int width = board.GetLength(1);
+
+            for (int i = 0; i < height; ++i)
+            {
+                for (int j = 0; j < width; ++j)
+                {
+                    Console.Write(board[i, j]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
