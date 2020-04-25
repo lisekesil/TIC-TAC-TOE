a0e2988df1824e65a282540b874cae16ac5ff6c9
Wojtek Lisowicz <lisekmadridista@gmail.com>
2020-04-25 19:00:05 +0200

    Add project files.

---
+++ TIC-TAC-TOE/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TIC_TAC_TOE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string namePlayerA = "";
+            string namePlayerB = "";
+            char symbolPlayerA = 'x';
+            char symbolPlayerB = 'o';
+
+            char[,] board = new char[3, 3]
+            {
+                {'1','2', '3' },
+                {'4','5', '6' },
+                {'7','8', '9' }
+            };
+
+            Console.Write("player A name: ");
+            namePlayerA = Console.ReadLine();
+            Console.Write("player B name: ");
+            namePlayerB = Console.ReadLine();
+        }
+    }
+}
